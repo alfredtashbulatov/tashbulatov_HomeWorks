51e0e63a9a5de33bd40e9286bd788c8f5dd54a29
Alfred <alfred101525@gmail.com>
2024-05-02 09:23:10 +0300

    гшкаргук

---
+++ lesson3/User.py
@@ -0,0 +1,26 @@
+class user:
+    first_name = ""
+    last_name = ""
+    
+    def __init__(self, first_name):
+        print("мое имя ", first_name)
+        self.first_name = first_name
+    
+    def lastname(self, last_name):
+        print("моя фамилия", last_name)
+        self.last_name = last_name
+
+    def flname(self):
+        print(self.last_name, self.first_name)    
+    
+    # def __init__(self, name):
+    #     print("Иеня зовут", name)
+    #     self.username = name
+
+    # def last_name(self, lastname):
+    #     print("моя фамилия", lastname)
+            
+
+alfred = user("alfred")
+alfred.lastname("tashbulatov")
+alfred.flname()
---
+++ lesson3/lesson3.py
@@ -0,0 +1,34 @@
+# class user:
+#     age = 0 
+#     name = "noname"
+#     email = "test@mail.com"
+
+# users = user()
+
+class user:
+    age = 0
+
+    def __init__(self, name):
+        print("я создался!!")
+        self.username = name
+        
+        
+    def sayname(self):
+        print("Меня зовут:", self.username)
+
+    def sayage(self):
+        print(self.age)
+
+    def setAge(self, newAge):
+        self.age = newAge
+        print(newAge)
+
+
+
+
+
+
+alfred = user("Альфред")        
+alfred.sayname()
+alfred.setAge(21)
+alfred.sayage
