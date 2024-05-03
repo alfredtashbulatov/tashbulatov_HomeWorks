9772ce6a2ded9518f336cf4ae5c5c8f7cb01208c
Alfred <alfred101525@gmail.com>
2024-05-03 13:55:12 +0300

    q1112

---
+++ lesson3/Address.py
@@ -0,0 +1,7 @@
+class address:
+    def __init__(self, inex, sity, street, house, apartment):
+        self.index = inex
+        self.sity = sity
+        self.street = street
+        self.house = house
+        self.apartment = apartment
