c535735f2019a4018928a522cb26dc010c550991
Alfred <alfred101525@gmail.com>
2024-05-07 21:20:04 +0300

    21"19

--- lesson4/test_string_utils.py
+++ lesson4/test_string_utils.py
@@ -3,71 +3,153 @@ from stringUtils import StringUtils
 
 stringUtils = StringUtils()
 
-def test_pozitive_capitilize():
+def capitilize(self, string: str) -> str:
     stringUtils = StringUtils()
-    def capitilize(self, string: str) -> str:
-        return string.capitalize()
+    return string.capitalize()
+def test_capitilize():
+    assert StringUtils().capitilize("skypro") == "Skypro"
+    assert StringUtils().capitilize("") == ""
 
-
-def test_pozitive_trim():
+def capitilize(self, string: str) -> str:
     stringUtils = StringUtils()
-    def trim(self, string: str) -> str:
-        whitespace = " "
-        while (string.startswith(whitespace)):
-            string = string.removeprefix(whitespace)
-        return string
+    return string.capitalize()
+def test_nig_capitilize():
+    assert StringUtils().capitilize("to day") == "today"
+    assert StringUtils().capitilize("") == ""    
 
+
+def trim(self, string: str) -> str:
+    stringUtils = StringUtils()
+    whitespace = " "
+    while (string.startswith(whitespace)):
+        string = string.removeprefix(whitespace)
+    return string
+def test_trim():
+    assert StringUtils().trim("  today") == "today"
+    assert StringUtils().trim("") == ""
+
+def trim(self, string: str) -> str:
+    stringUtils = StringUtils()
+    whitespace = " "
+    while (string.startswith(whitespace)):
+        string = string.removeprefix(whitespace)
+    return string
+def test_nig_trim():
+    assert StringUtils().trim("  to day") == "today"
+    assert StringUtils().trim("") == ""
+
+
+def to_list(self, string: str, delimeter = ",") -> list[str]:
+    strungUtils = StringUtils()
+    if(self.is_empty(string)):
+        return []
 def test_to_list():
+    assert StringUtils().to_list("1, 2, 3") == ["1", "2", "3"]
+    # assert StringUtils().to_list("") == ["", ""]
+             
+def contains(self, string: str, symbol: str) -> bool:
     strungUtils = StringUtils()
-    def to_list(self, string: str, delimeter = ",") -> list[str]:
-         if(self.is_empty(string)):
-            return []
+    res = False
+    try:
+        res = string.index(symbol) > -1
+    except ValueError:
+        pass
 
-
+    return res
 def test_contains():
+    assert StringUtils().contains("SkyPro", "S") == True
+
+def contains(self, string: str, symbol: str) -> bool:
     strungUtils = StringUtils()
-    def contains(self, string: str, symbol: str) -> bool:
-        res = False
-        try:
-            res = string.index(symbol) > -1
-        except ValueError:
-            pass
+    res = False
+    try:
+        res = string.index(symbol) > -1
+    except ValueError:
+        pass
 
-        return res
+    return res
+def test_contains_nig():
+    assert StringUtils().contains("SkyPro", "u") == False
 
+def delete_symbol(self, string: str, symbol: str) -> str:
+    strungUtils = StringUtils()
+    if(self.contains(string, symbol)):
+        string = string.replace(symbol, "")    
+    return string
 def test_delete_symbol():
+    assert StringUtils().delete_symbol("SkyPro", "k") == "SyPro"
+
+def delete_symbol(self, string: str, symbol: str) -> str:
     strungUtils = StringUtils()
-    def delete_symbol(self, string: str, symbol: str) -> str:
-        if(self.contains(string, symbol)):
-            string = string.replace(symbol, "")    
-        return string
-    
-def test_starts_with(): 
-    strungUtils = StringUtils()
-    def starts_with(self, string: str, symbol: str) -> bool:
-        return string.startswith(symbol)
-    
+    if(self.contains(string, symbol)):
+        string = string.replace(symbol, "")    
+    return string
+def test_delete_symbol_nig():
+    assert StringUtils().delete_symbol("SkyPro", "Pro") == "Sky"
+
+def starts_with(self, string: str, symbol: str) -> bool:
+    stringUtils = StringUtils()
+    return string.startswith(symbol)
+def test_starts_with():
+    assert StringUtils().starts_with("SkyPro", "S") == True
+
+def starts_with(self, string: str, symbol: str) -> bool:
+    stringUtils = StringUtils()
+    return string.startswith(symbol)
+def test_starts_with_nig():
+    assert StringUtils().starts_with("SkyPro", "P") == False 
+
+def end_with(self, string: str, symbol: str) -> bool:
+    stringUtils = StringUtils()
+    return string.endswith(symbol)
 def test_end_with():
-    strungUtils = StringUtils()
-    def end_with(self, string: str, symbol: str) -> bool:
-        return string.endswith(symbol)
+    assert StringUtils().end_with("SkyPro", "o") == True
 
+def end_with(self, string: str, symbol: str) -> bool:
+    stringUtils = StringUtils()
+    return string.endswith(symbol)
+def test_end_with_nig():
+    assert StringUtils().end_with("SkyPro", "y") == False
+
+def is_empty(self, string: str) -> bool:
+    stringUtils = StringUtils
+    string = self.trim(string)
+    return string == ""
 def test_is_empty():
-    strungUtils = StringUtils()
-    def is_empty(self, string: str) -> bool:
-        string = self.trim(string)
-        return string == ""
+    assert StringUtils().end_with("") == True
 
+def is_empty(self, string: str) -> bool:
+    stringUtils = StringUtils
+    string = self.trim(string)
+    return string == ""
+def test_is_empty_nig():
+    assert StringUtils().end_with("SkyPro") == False  
+
+def list_to_string(self, lst: list, joiner=", ") -> str:
+    stringUtils = StringUtils
+    string = ""
+    length = len(lst)
+        
+    if length == 0: 
+        return string 
+        
+    for i in range(0, length-1):
+        string += str(lst[i]) + joiner
+        
+    return string + str(lst[-1])
 def test_list_to_string():
-    strungUtils = StringUtils()
-    def list_to_string(self, lst: list, joiner=", ") -> str:
-        string = ""
-        length = len(lst)
+    assert StringUtils().list_to_string([1,2,3,4]) == "1, 2, 3, 4"
+def list_to_string(self, lst: list, joiner=", ") -> str:
+    stringUtils = StringUtils
+    string = ""
+    length = len(lst)
         
-        if length == 0: 
-            return string 
+    if length == 0: 
+        return string 
         
-        for i in range(0, length-1):
-            string += str(lst[i]) + joiner
+    for i in range(0, length-1):
+        string += str(lst[i]) + joiner
         
-        return string + str(lst[-1])    
+    return string + str(lst[-1])
+def test_list_to_string_nig():
+    assert StringUtils().list_to_string(["Sky", "Pro"], "-") == "Sky-Pro"
