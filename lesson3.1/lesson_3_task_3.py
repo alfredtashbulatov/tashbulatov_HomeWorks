9eaf113e6bc19548b5a6f964223ddc714cd35034
alfredtashbulatov <152917111+alfredtashbulatov@users.noreply.github.com>
2024-05-19 15:28:02 +0300

    drdr

--- lesson3.1/lesson_3_task_3.py
+++ lesson3.1/lesson_3_task_3.py
@@ -1,11 +1,11 @@
 from address import Address
-from mailing import mailing
+from mailing import Mailing
 
-to_address = address('111024','Москва', 'Душинская', '16', '25')
-from_address = address('122445', 'Уфа', 'Проспект октября', '15', '56')
-Mailing = mailing(to_address, from_address, 1234, 'Коробка 500гр')
+to_address = Address('111024','Москва', 'Душинская', '16', '25')
+from_address = Address('122445', 'Уфа', 'Проспект октября', '15', '56')
+Mailing = Mailing(to_address, from_address, 1234, 'Коробка 500гр')
 
-print(f"Отправление {Mailing.track} из {Mailing.from_address.index}, {Mailing.from_address.sity,}"
+print(f"Отправление {Mailing.track} из {Mailing.from_address.index}, {Mailing.from_address.Sity,}"
       f"{Mailing.from_address.street}, {Mailing.from_address.house} - {Mailing.from_address.apartment}"
-      f"{Mailing.to_address.index}, {Mailing.to_address.sity}, {Mailing.to_address.street}, {Mailing.to_address.house} - {Mailing.to_address.apartment}"
-      f"Стоимость {Mailing.cots} Рублей")
+      f"{Mailing.to_address.index}, {Mailing.to_address.Sity}, {Mailing.to_address.street}, {Mailing.to_address.house} - {Mailing.to_address.apartment}"
+      f"Стоимость {Mailing.Cots} Рублей")
