1bbcc8014db2e7b1382c891edb5635f3dcb29201
alfredtashbulatov <152917111+alfredtashbulatov@users.noreply.github.com>
2024-05-20 07:53:38 +0300

    bvb

--- lesson3.1/lesson_3_task_3.py
+++ lesson3.1/lesson_3_task_3.py
@@ -8,4 +8,4 @@ Mailing = mailing(to_address, from_address, 1234, 'Коробка 500гр')
 print(f"Отправление {Mailing.track} из {Mailing.from_address.index}, {Mailing.from_address.sity,}"
       f"{Mailing.from_address.street}, {Mailing.from_address.house} - {Mailing.from_address.apartment}"
       f"{Mailing.to_address.index}, {Mailing.to_address.sity}, {Mailing.to_address.street}, {Mailing.to_address.house} - {Mailing.to_address.apartment}"
-      f"Стоимость {Mailing.cots} Рублей")
+      f"Стоимость {Mailing.Cots} Рублей")
