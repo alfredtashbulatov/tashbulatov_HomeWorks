d6631f851068a58376db86e4d95d9d3fddf9c8c1
Alfred <alfred101525@gmail.com>
2024-06-08 12:30:37 +0300

    cvbnm

--- lesson6.1/test_calculator.py
+++ lesson6.1/test_calculator.py
@@ -9,32 +9,30 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 driver = webdriver.Chrome()
-driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
-input = driver.find_element(By.CSS_SELECTOR, "#delay")
-input.clear()
-input.send_keys("45")
-sleep(3)
 
-driver.find_element(By.XPATH, "//span[text() = '7']").click
-driver.find_element(By.XPATH, "//span[text() = '+']").click
-driver.find_element(By.XPATH, "//span[text() = '8']").click
-driver.find_element(By.XPATH, "//span[text() = '=']").click
-sleep(5)
-WebDriverWait(driver, 46).until(EC.text_to_be_present_in_element(By.CLASS_NAME, "screen"), "15")
-result = driver.find_element(By.CLASS_NAME, "screen").text
-assert result == "15"
-# def test_calculator_from():
-#     driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
-#     input = driver.find_element(By.CSS_SELECTOR, "#delay")
-#     input.clear()
-#     input.send_keys("45")
-#     sleep(3)
+def open_calculator():
+    driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
 
-#     driver.find_element(By.XPATH, "//span[text() = '7']").click
-#     driver.find_element(By.XPATH, "//span[text() = '+']").click
-#     driver.find_element(By.XPATH, "//span[text() = '8']").click
-#     driver.find_element(By.XPATH, "//span[text() = '=']").click
+def input_data():
+    input = driver.find_element(By.CSS_SELECTOR, "#delay")
+    input.clear()
+    input.send_keys("45")
 
-#     WebDriverWait(driver, 46).until(EC.text_to_be_present_in_element(By.CLASS_NAME, "screen"), "15")
-#     result = driver.find_element(By.CLASS_NAME, "screen").text
-#     assert result == "15"
+def button_click():
+    driver.find_element(By.XPATH, '//span[text()="7"]').click()
+    driver.find_element(By.XPATH, '//span[text()="+"]').click()
+    driver.find_element(By.XPATH, '//span[text()= "8"]').click()
+    driver.find_element(By.XPATH, '//span[text()= "="]').click()
+
+def data_checking():
+    WebDriverWait(driver, 46).until(
+        EC.text_to_be_present_in_element((By.CSS_SELECTOR, 'div[class="screen"]'), "15"))
+    result = driver.find_element(By.CSS_SELECTOR, 'div[class="screen"]').text
+    assert result == "15"
+
+def test_calculator():
+    open_calculator()
+    input_data()
+    button_click()
+    data_checking()
+    driver.quit()
--- lesson6.1/test_shop.py
+++ lesson6.1/test_shop.py
@@ -1,5 +1,6 @@
-import pytest
 from time import sleep
+from typing import Literal
+import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
@@ -7,22 +8,41 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-def test_shop_from(chrome_browser):
-    chrome_browser.get("https://www.saucedemo.com/")
-    chrome_browser.find_element(By.ID, "#user-name").send_keys("standard_user")
-    chrome_browser.find_element(By.ID, "#password").send_keys("secret_sauce")
-    chrome_browser.find_element(By.ID, "#login-button").click()
-    chrome_browser.find_element(By.ID, "#add-to-cart-sauce-labs-backpack").click()
-    chrome_browser.find_element(By.ID, "#add-to-cart-sauce-labs-bolt-t-shirt").click()
-    chrome_browser.find_element(By.ID, "#add-to-cart-sauce-labs-onesie").click()
-    chrome_browser.find_element(By.ID, "#shopping_cart_container").click()
-    chrome_browser.find_element(By.ID, "#checkout").click()
-    chrome_browser.find_element(By.ID, "#first-name").send_keys("alfred")
-    chrome_browser.find_element(By.ID, "#first-name").send_keys("tashbulatov")
-    chrome_browser.find_element(By.ID, "#postal-code").send_keys("450017")
-    chrome_browser.find_element(By.ID, "#continue").click()
-    total_price = chrome_browser.find_element(By.CLASS_NAME, "summary_total_label")
-    total = total_price.text.sprit().replace("Total: $ ", "")
+driver = webdriver.Chrome()
 
-    expected_total = "58.29"
-    assert total == expected_total
+def open_shop():
+    driver.get('https://www.saucedemo.com/')
+
+def log_in():
+    driver.find_element(By.CSS_SELECTOR, "#user-name").send_keys("standard_user")
+    driver.find_element(By.CSS_SELECTOR, "#password").send_keys("secret_sauce")
+    driver.find_element(By.CSS_SELECTOR, "#login-button").click()  
+
+def add_to_cart():
+    driver.find_element(By.CSS_SELECTOR, "#add-to-cart-sauce-labs-backpack").click()
+    driver.find_element(By.CSS_SELECTOR, "#add-to-cart-sauce-labs-bolt-t-shirt").click()
+    driver.find_element(By.CSS_SELECTOR, "#add-to-cart-sauce-labs-onesie").click()
+
+def go_to_card():  
+    driver.find_element(By.CSS_SELECTOR, "#shopping_cart_container").click()  
+
+def input_order():
+    driver.find_element(By.CSS_SELECTOR, "#checkout").click()
+    driver.find_element(By.CSS_SELECTOR, "#first-name").send_keys("alfred")
+    driver.find_element(By.CSS_SELECTOR, "#last-name").send_keys("tashbulatov")
+    driver.find_element(By.CSS_SELECTOR, "#postal-code").send_keys("450017")
+    driver.find_element(By.CSS_SELECTOR, "#continue").click()
+
+def price_check():
+    total_price = driver.find_element(By.CSS_SELECTOR, 'div[class="summary_total_label"]').text
+    assert total_price == str("Total: $58.29")
+
+
+def test_shop():
+    open_shop()
+    log_in()
+    add_to_cart()
+    go_to_card()
+    input_order()
+    price_check()
+    driver.quit()
