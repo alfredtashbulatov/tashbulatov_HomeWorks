2c5610de9424597f41a590ecc886772e79f66d5e
Alfred <alfred101525@gmail.com>
2024-06-18 23:10:16 +0300

    less8.1

--- lesson8/Clients_method.py
+++ lesson8/Clients_method.py
@@ -63,7 +63,7 @@ class metods:
         resp = requests.patch(self.url + "/company/status/" + str(id), headers=my_headers, json={"isActive": isActive})
         return resp.json()
     
-# получить список сотрудников
+# получить список сотрудников для компании
     def employee_list_company(self, id):
         my_headers = {}
         my_headers["x-client-token"] = self.auth()
@@ -71,16 +71,16 @@ class metods:
         return resp.json()
 
 # добавление сотрудника 
-    def add_employee(self, new_id):
+    def add_employee(self, new_id, firstName, lastName, phone):
         data_empl = {
             "id": 1111,
-            "firstName": "Alik",
-            "lastName": "Drogin",
-            "middleName": "Ashot",
+            "firstName": firstName,
+            "lastName": lastName,
+            "middleName":  None,
             "companyId": new_id,
             "email": None,
             "url": None,
-            "phone": "4 534 651 34 67",
+            "phone": phone,
             "birthdate": None,
             "isActive": True
             }
@@ -90,10 +90,24 @@ class metods:
         return resp.json()
     
     # поиск сотрудника по id
-    def search_employee_by_id(self, emp_id):
+    def search_employee_by_id(self, id):
+        # result = requests.get(self.url + "/employee/", str(emp_id))
+        # return result.json()
         my_headers = {}
         my_headers["x-client-token"] = self.auth()
-        resp = requests.get(self.url + "/employee", json={"id": emp_id})
+        resp = requests.get(self.url + "/employee/" + str(id), headers=my_headers)
         return resp.json()
-        
-        
+    
+    # изменить информацию о сотруднике 
+    def edit_info_employee(self, id, new_name, new_mail, new_phone, isActive):
+        edit_emp =  {
+            "lastName": new_name,
+            "email": new_mail,
+            "url": None,
+            "phone": new_phone,
+            "isActive": isActive
+            }   
+        my_headers = {}
+        my_headers["x-client-token"] = self.auth() 
+        resp = requests.patch(self.url + "/employee/" + str(id), headers=my_headers, json=edit_emp)
+        return resp.json()
--- lesson8/test_lesson8.py
+++ lesson8/test_lesson8.py
@@ -31,7 +31,7 @@ def test_edit_company():
     assert edit["description"] == new_desr
 
 # поиск конкретной компании
-def test_search_company_dy_id():
+def test_search_company_by_id():
     name = "ruderius"
     desc = "labirint-cyti"
     res= metod.create_company(name, desc)
@@ -47,51 +47,64 @@ def test_diactivate_company():
     new_id = res["id"]
     result = metod.control_company(new_id, False)
     assert result["isActive"] == False
+    assert result["id"] == new_id
 
 # получение списка сотрудников 
 def test_employee_list_company():
     body = metod.take_list_company()
     id = body[-1]["id"]
     res = metod.employee_list_company(id)
+    assert len(res) > 0
+    
+    
 
     
 # добавление сотрудника 
-def test_add_employe():
-    name = "alik"
-    desc = "tashbulatov"
+def test_add_employe_in_company():
+    name = "test mp3"
+    desc = "nwe rosk"
+    firstName = "vadik"
+    lastName = "shageev"
+    phone = "9 999 999 99 99"
     res = metod.create_company(name, desc)
     new_id = res["id"]
-    resp = metod.add_employee(new_id)
+    resp = metod.add_employee(new_id, firstName, lastName, phone)
     emp_id = resp["id"]
     assert new_id == res["id"]
     assert emp_id == resp['id']
     
 
     # поиск сотрудника по id
-
 def test_search_employee_by_id():
-    # получим список компаний 
-    body = metod.take_list_company()
-    id = body[0]["id"]
-    # получим список сотрудников для компании
-    resp = metod.employee_list_company(id)
-    emp_id = resp["id"]
-    result = metod.search_employee_by_id(emp_id)
-    # r = result["id"]
-    # assert r == emp_id
-    
-
-    
-# смоук тетс добавление сотрудниа в компанию
-def test_smoyk_add_employee():
-    name = "new company"
-    desc = ""
-    res= metod.create_company(name, desc)
+    name = "test mp4"
+    desc = "nwe rosk"
+    firstName = "oleg"
+    lastName = "shageev"
+    phone = "8 888 888 88 88"
+    res = metod.create_company(name, desc)
     new_id = res["id"]
-    # создаем сотрудника 
-    resp = metod.add_employee(new_id)
+    resp = metod.add_employee(new_id, firstName, lastName, phone)
     emp_id = resp["id"]
     result = metod.search_employee_by_id(emp_id)
+    assert phone  == result["phone"]
+    assert new_id == result["companyId"]
+    assert firstName == result["firstName"]
 
-
+# изменение данных сотрудника   
+def test_edit_info_employee():  
+    name = "test mp4"
+    desc = "nwe rosk"
+    firstName = "oleg"
+    lastName = "shageev"
+    phone = "8 888 888 88 88"
+    res = metod.create_company(name, desc)
+    new_id = res["id"]
+    resp = metod.add_employee(new_id, firstName, lastName, phone)
+    emp_id = resp["id"]
+    new_name =  "Evgeniy"
+    new_mail =  None
+    new_phone = "7 777 777 77 77"
+    isActive = True
+    edit_emp = metod.edit_info_employee(emp_id, new_name, new_mail, new_phone, isActive) 
+    
 
